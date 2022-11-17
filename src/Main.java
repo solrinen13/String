16b37795a48e2cbc243017602e29c1531f8913d8
Saveliy <saveliy.kan@gmail.com>
2022-11-17 21:20:45 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        // 1 задача
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("“ФИО сотрудника- " + fullName + "“");
+
         // 2 задача
         String result = fullName.toUpperCase();
         System.out.println("“Данные ФИО сотрудника для заполнения отчета — " + result + "“");
 
          // 3 задача
-
         String fullNameTwo = "Иванов Семён Семёнович";
         fullNameTwo = fullNameTwo.replace("ё","е");
         System.out.println("”Данные ФИО сотрудника — "+fullNameTwo+"”");
